b420f2854250d0aae124d5720c7d12792c8e53eb
Rooting21 <lpzockerrmc@gmail.com>
2017-05-02 15:23:19 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+apt-get update 
+apt-get upgrade -y
+apt-get install serverstats
+cd /usr/share/doc/serverstats/
+cp examples/apache.conf /etc/apache2/sites-available/serverstats.conf
+a2ensite serverstats.conf
+service apache2 reload
