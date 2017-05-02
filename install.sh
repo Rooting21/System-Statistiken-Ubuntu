e43462d43c290846b206a39f6786cefe9603c562
Rooting21 <lpzockerrmc@gmail.com>
2017-05-02 15:32:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt-get update 
 apt-get upgrade -y
-apt-get install serverstats
+apt-get install serverstats -y
 cd /usr/share/doc/serverstats/
 cp examples/apache.conf /etc/apache2/sites-available/serverstats.conf
 a2ensite serverstats.conf
